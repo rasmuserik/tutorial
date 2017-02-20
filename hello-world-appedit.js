8144be9b7ec64cd021d8a735b89299f4b70e9711
RasmusErik Voel Jensen <github-rasmuserik@solsort.dk>
2017-02-20 21:02:30 +0100

    Create hello-world-appedit.js

---
+++ hello-world-appedit.js
@@ -0,0 +1,22 @@
+// # AppEdit Hello World
+//
+// Modules/libraries are loaded with `require(...)`.
+//
+// Documentation/source for the solsort library can be read on:
+// <https://appedit.solsort.com/?page=read&github=solsort/solsort>
+
+ss = require('solsort');
+
+// Render the ui reactively
+
+ss.html(() => ['div',
+  ['h1', 'Hello world'],
+  ['p', 'Count: ', ss.getJS('count', 0)],
+  ['button', {onClick: ss.event('increment')},
+    'Increment']);
+
+// Handler for button clicks
+
+ss.handle('increment', () => 
+  ss.setJS('count', ss.getJS('count', 0) + 1));
+  
